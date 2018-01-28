4d06e4aafae6c415b348f326bcf023ca109c2208
alar77 <19211824+alar77@users.noreply.github.com>
2018-01-29 00:30:45 +0100

    autoreconf instead of autoconf in the second step

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-if [ ! -L install-sh ] ; then
-autoconf && aclocal & automake --add-missing && autoconf 
-fi
-autoreconf && ./configure && make
+autoconf && aclocal & automake --add-missing && autoreconf && ./configure && make
 
