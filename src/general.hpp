ee4edeee308d22ae99c600c31f7870b23971b692
alar77 <19211824+alar77@users.noreply.github.com>
2018-01-28 20:23:46 +0100

    General header for true cpp files

---
+++ src/general.hpp
@@ -0,0 +1,19 @@
+/*
+ * general.hpp
+ *
+ *  Created on: 28 gen 2018
+ *      Author: giovanni
+ *
+ * Licensed Material - Property of Hex Keep s.r.l.
+ * (c) Copyright Hex Keep s.r.l. 2012-2014
+ */
+
+#ifndef GENERAL_HPP_
+#define GENERAL_HPP_
+#include <string>
+using std::string;
+
+
+
+
+#endif /* GENERAL_HPP_ */
