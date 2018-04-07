551f953f40f62d0f56c18dfae05a3ad504c2638b
francesco sabatini <info@francescosabatini.com>
2018-04-07 07:00:40 +0200

    fix frase anti-vigliacchi

--- src/fight.cpp
+++ src/fight.cpp
@@ -2215,10 +2215,9 @@ int DamageEpilog( struct char_data* ch, struct char_data* victim,
 		}
 		else {
             die( victim, killedbytype, ch );
-            sprintf( buf, "%s e` mort%s vigliaccamente combattendo con %s a %s\n\r",
+            sprintf( buf, "\n%s e` mort%s $c0001vigliaccamente$c0007 combattendo contro %s!\n\r",
 							 GET_NAME(victim), SSLF( victim ),
-							 GET_NAME(ch),victim->in_room > -1 ?
-							 (real_roomp(victim->in_room))->name : "Nowhere" );
+							 GET_NAME(ch));
 				
             send_to_all(buf);
 			
