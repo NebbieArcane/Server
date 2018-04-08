5b4c0b26356cf7445dcc309c1ce7a0aa2c7378a4
francesco sabatini <info@francescosabatini.com>
2018-04-08 16:46:04 +0200

    typo fix

--- src/spec_procs.cpp
+++ src/spec_procs.cpp
@@ -759,7 +759,7 @@ int ThiefGuildMaster( struct char_data* ch, int cmd, char* arg,
 		}
 		else {
 			number = old_search_block(arg,0,strlen(arg),n_skills,FALSE);
-			send_to_char ("Il Masestro dei Ladri dice ",ch);
+			send_to_char ("Il Maestro dei Ladri dice ",ch);
 
 			if (number == -1) {
 				send_to_char("'Non conosco questa abilita'.'\n\r", ch);
