bbf91d5eac7a3995853ca7984a365d5a6011e808
alar77 <19211824+alar77@users.noreply.github.com>
2018-04-25 20:56:08 +0200

    Fixed log files size

--- src/logging.cpp
+++ src/logging.cpp
@@ -69,8 +69,8 @@ log4cxx::LoggerPtr log_configure(log4cxx::LoggerPtr &loggerInstance,string logna
 	log4cxx::LayoutPtr l(new log4cxx::PatternLayout(LAYOUT_2));
 	log4cxx::RollingFileAppenderPtr r(new log4cxx::RollingFileAppender(l, logfile,append));
 	r->setMaxBackupIndex(numLogs);
-	log4cxx::LogString size("200M");
-	r->setMaxFileSize("200M");
+	log4cxx::LogString size("200MB");
+	r->setMaxFileSize("200MB");
 	r->setBufferedIO(false); // We depend on the final log line being always written.
 	r->setBufferSize(1024);
 	r->activateOptions(p);
