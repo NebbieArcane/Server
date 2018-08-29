bd6c77a03e6fafd6707e80a95df74c33dfa63c84
Francesco Sabatini <francesco.sabatini.wm@hotmail.it>
2018-08-29 03:44:20 +0200

    Nuovo caso
    Aggiunto il caso !IS_AFFECTED(i, AFF_GLOBE_DARKNESS) al controllo IF

--- src/spec_procs3.cpp
+++ src/spec_procs3.cpp
@@ -3202,7 +3202,8 @@ OBJSPECIAL_FUNC(AntiSunItem) {
 
 	if(OUTSIDE(ch) && weather_info.sunlight == SUN_LIGHT &&
 			weather_info.sky<= SKY_CLOUDY &&
-			!affected_by_spell(ch, SPELL_GLOBE_DARKNESS)) {
+       !affected_by_spell(ch, SPELL_GLOBE_DARKNESS) &&
+       !IS_AFFECTED(i, AFF_GLOBE_DARKNESS)) {
 		/* frag the item! */
 		act("The sun strikes $p, causing it to fall appart!", FALSE, ch, obj,
 			NULL, TO_CHAR);
