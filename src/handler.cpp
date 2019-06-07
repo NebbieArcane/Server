4b0d7afd960054ce048de7825d44dd48e6a75cd6
Corrado Serafini <corra@Corra.lan>
2019-06-08 00:23:33 +0200

    FixQuestLinkDead

--- src/handler.cpp
+++ src/handler.cpp
@@ -2287,16 +2287,23 @@ void extract_char_smarter(struct char_data* ch, long save_room) {
         ch->specials.eq_val_idx = 0;
     }
 
-    if(IS_PC(ch) && ch->specials.quest_ref != NULL)
+    if(ch->specials.quest_ref != NULL)
     {
-        send_to_char("$c0011Mi dispiace, hai fallito la tua quest!\n\r", ch);
-        mudlog(LOG_PLAYERS, "%s has failed the quest!", GET_NAME(ch));
-
-        if(real_roomp((ch->specials.quest_ref)->in_room)->people)
+        if(IS_PC(ch))
         {
-            act("\n\r$c0014$n$c0014 ha perso il senso della sua esistenza...$c0007", FALSE, ch->specials.quest_ref, 0, 0, TO_ROOM);
+            send_to_char("$c0011Mi dispiace, hai fallito la tua quest!\n\r", ch);
+            mudlog(LOG_PLAYERS, "%s has failed the quest!", GET_NAME(ch));
+
+            if(ch->specials.quest_ref)
+            {
+                if(real_roomp((ch->specials.quest_ref)->in_room)->people)
+                {
+                    act("\n\r$c0014$n$c0014 ha perso il senso della sua esistenza...$c0007", FALSE, ch->specials.quest_ref, 0, 0, TO_ROOM);
+                }
+            }
+            extract_char(ch->specials.quest_ref);
         }
-        extract_char(ch->specials.quest_ref);
+
         ch->specials.quest_ref = NULL;
     }
 
