afcdc491c0cdb0d5c303ed0be1a360da49ec7393
Montero <corra72@gmail.com>
2021-03-11 17:33:00 +0100

    Update act.obj2.cpp
    I mob non usano più gli oggetti personal dei pg.

--- src/act.obj2.cpp
+++ src/act.obj2.cpp
@@ -672,7 +672,7 @@ void wear(struct char_data* ch, struct obj_data* obj_object, long keyword) {
         tch = ch->desc->original;
     }
 
-    if(IS_OBJ_STAT2(obj_object, ITEM2_PERSONAL) && !IS_IMMORTAL(ch) && IS_PC(ch) && !pers_on(ch,obj_object))
+    if(IS_OBJ_STAT2(obj_object, ITEM2_PERSONAL) && !IS_IMMORTAL(ch) && !pers_on(ch,obj_object))
     {
         sprintf(buffer, "Non puoi usare %s, non ti appartiene!\n\r", obj_object->short_description);
         send_to_char(buffer, ch);
